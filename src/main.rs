228d2875dd5b489c439fc1ac76211750a5a5a77f
kattenelvis <officialkattenelvis@gmail.com>
2025-12-22 12:36:36 +0100

    aworking voting system

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 // }
 
 fn main() {
-    const N: i32 = 10_i32.pow(6);
     // let candidates = ["a", "b", "c"];
 
     // let preferences = [["a"], ["b"]];
@@ -17,14 +16,36 @@ fn main() {
     //     println!("{}", p[i]);
     // }
 
-    let p: [[i32; 3]; 10] = std::array::from_fn(|n| match n {
-        val if (n % 3 == 0) => [0, 1, 2],
-        _ => [1, 0, 2],
-    });
+    const N: usize = 10_usize.pow(8);
 
-    println!("{p:?}");
+    let mut p: Vec<[i32; 3]> = Vec::with_capacity(N);
+    for n in 0..N {
+        p.push(if n % 4 == 0 { [0, 1, 2] } else { [1, 0, 2] });
+    }
 
-    // Voting
+    // let p: Box<[[i32; 3]; N]> = Box::new(std::array::from_fn(|n| match n {
+    //     _ if (n % 3 == 0) => [0, 1, 2],
+    //     _ => [1, 0, 2],
+    // }));
+
+    // println!("{p:?}");
+
+    // Plurality Voting (FPTP)
+    let mut sums = [0, 0, 0];
+    for n in p {
+        match n[0] {
+            0 => sums[0] += 1,
+            1 => sums[1] += 1,
+            2 => sums[2] += 2,
+            _ => {}
+        }
+
+        // if n[0] == 0 {
+        //     sums[0] += 1;
+        // };
+    }
+
+    println!("{sums:?}");
 
     // let mut counts = [0, 0, 0];
     // for i in 0..10000 {}
