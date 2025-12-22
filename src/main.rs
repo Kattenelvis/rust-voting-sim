4928ee184a621e066b6016f41ce55acf784c60ca
kattenelvis <officialkattenelvis@gmail.com>
2025-12-22 13:17:40 +0100

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,24 @@
-// fn preferenceAssignment() {
-// }
-
 fn main() {
-    // let candidates = ["a", "b", "c"];
+    // Voting population size
+    const N: usize = 10_usize.pow(7);
 
-    // let preferences = [["a"], ["b"]];
+    let p = preference_loading(N);
+
+    plurality_voting(p);
+}
+
+fn preference_loading(n: usize) -> Vec<[i32; 3]> {
     // Loading preference data into use
-    // let mut pref: [str; 10000] = Vec::with_capacity(10000);
-
-    // let mut p = Vec::with_capacity(N);
-    // let mut p: Vec<_> = (0..N).collect();
-    // println!("{p:?}");
-    // for i in 0..N {
-    //     p.push(["a", "b", "c"]);
-    //     println!("{}", p[i]);
-    // }
-
-    const N: usize = 10_usize.pow(8);
-
-    let mut p: Vec<[i32; 3]> = Vec::with_capacity(N);
-    for n in 0..N {
+    // Assumes transitive and complete preferences (total ordering)
+    let mut p: Vec<[i32; 3]> = Vec::with_capacity(n);
+    for n in 0..n {
         p.push(if n % 4 == 0 { [0, 1, 2] } else { [1, 0, 2] });
     }
 
-    // let p: Box<[[i32; 3]; N]> = Box::new(std::array::from_fn(|n| match n {
-    //     _ if (n % 3 == 0) => [0, 1, 2],
-    //     _ => [1, 0, 2],
-    // }));
+    return p;
+}
 
-    // println!("{p:?}");
-
+fn plurality_voting(p: Vec<[i32; 3]>) {
     // Plurality Voting (FPTP)
     let mut sums = [0, 0, 0];
     for n in p {
@@ -39,15 +28,31 @@ fn main() {
             2 => sums[2] += 2,
             _ => {}
         }
-
-        // if n[0] == 0 {
-        //     sums[0] += 1;
-        // };
     }
 
     println!("{sums:?}");
-
-    // let mut counts = [0, 0, 0];
-    // for i in 0..10000 {}
-    // println!("Hello, world!");
 }
+
+// fn preferenceAssignment() {
+// }
+
+// let candidates = ["a", "b", "c"];
+
+// let preferences = [["a"], ["b"]];
+// Loading preference data into use
+// let mut pref: [str; 10000] = Vec::with_capacity(10000);
+
+// let mut p = Vec::with_capacity(N);
+// let mut p: Vec<_> = (0..N).collect();
+// println!("{p:?}");
+// for i in 0..N {
+//     p.push(["a", "b", "c"]);
+//     println!("{}", p[i]);
+// }
+
+// let p: Box<[[i32; 3]; N]> = Box::new(std::array::from_fn(|n| match n {
+//     _ if (n % 3 == 0) => [0, 1, 2],
+//     _ => [1, 0, 2],
+// }));
+
+// println!("{p:?}");
