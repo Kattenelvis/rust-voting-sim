419c6f8f82edf832d4f5a80a3d02cf1e7ce728ac
kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2025-12-31 20:21:50 +0100

    Sketching KPIs

---
+++ src/kpi.rs
@@ -0,0 +1,14 @@
+pub mod kpi {
+    // A KPI can be a generic <T> so long as it implements a maximum and minimum value (So a
+    // partial ordering?)
+    // So long as it's a total ordering... no let's make it more general
+    // It needs to implement
+    // uhhhh
+    // It can definetly be any signed or unsigned integer
+    // any enum if there's a match case for matchigng each case to a value.
+
+    pub fn main() -> i128 {
+        let a: i128 = 1;
+        a
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,23 @@ use code_timing_macros::time_function;
 use std::sync::Arc;
 use std::thread;
 
+mod kpi;
+
 #[time_function]
 fn main() {
+    let n = kpi::kpi::main();
+    println!("{n:?}");
     // Voting population size
-    const N: u32 = 10_u32.pow(8);
+    const N: u32 = 2 * 10_u32.pow(5);
 
     let preferences: Arc<Vec<[u8; 3]>> = Arc::new(preference_loading(N));
     let preferences_clone_1 = Arc::clone(&preferences);
     let preferences_clone_2 = Arc::clone(&preferences);
+    let preferences_clone_3 = Arc::clone(&preferences);
 
     let mut vote_plurality = [0; 3];
-    let mut vote_copeland = [0; 3];
+    let mut vote_score = [0; 3];
+    let mut vote_quadratic = [0; 3];
 
     let h1 = thread::spawn(move || {
         vote_plurality = plurality_voting(&preferences_clone_1);
@@ -21,13 +27,20 @@ fn main() {
     });
 
     let h2 = thread::spawn(move || {
-        vote_copeland = copeland_voting(&preferences_clone_2);
-        println!("{vote_copeland:?}");
-        vote_copeland
+        vote_score = score_voting(&preferences_clone_2);
+        println!("{vote_score:?}");
+        vote_score
+    });
+
+    let h3 = thread::spawn(move || {
+        vote_quadratic = quadratic_voting(&preferences_clone_3);
+        println!("{vote_quadratic:?}");
+        vote_quadratic
     });
 
     h1.join().unwrap();
     h2.join().unwrap();
+    h3.join().unwrap();
 }
 
 fn preference_loading(population: u32) -> Vec<[u8; 3]> {
@@ -59,14 +72,25 @@ fn plurality_voting(p: &Vec<[u8; 3]>) -> [usize; 3] {
     return sums;
 }
 
-fn copeland_voting(preferences: &Vec<[u8; 3]>) -> [usize; 3] {
+fn quadratic_voting(p: &Vec<[u8; 3]>) -> [usize; 3] {
+    let mut sums = [0, 0, 0];
+
+    for preferences in p {
+        for i in 0..preferences.len() {
+            sums[i] += (preferences.len() - i - 1).pow(2);
+        }
+    }
+    sums
+}
+
+fn score_voting(preferences: &Vec<[u8; 3]>) -> [usize; 3] {
     let mut sums = [0, 0, 0];
     for preference in preferences {
         for i in 0..preference.len() {
             sums[preference[i] as usize] += preference.len() - i - 1;
         }
     }
-    return sums;
+    sums
 }
 
 // fn symmetric_borda_score(preferences: Vec<[usize; 3]>) -> [usize; 3] {}
