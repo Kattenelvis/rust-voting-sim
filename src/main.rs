7046a6bc35e4e29739716ef7e9eb6b7eb65a7e5c
kattenelvis <53380638+Kattenelvis@users.noreply.github.com>
2026-01-03 20:20:44 +0100

    Added cursed code the compiler wanted me to add will probably remove

--- src/main.rs
+++ src/main.rs
@@ -8,41 +8,12 @@ mod preference_loading;
 
 #[time_function]
 fn main() {
-    let n = kpi::kpi::main();
-    println!("{n:?}");
-    // Voting population size
-    const N: u32 = 2 * 10_u32.pow(5);
+    let world: World<i32> = World {
+        population: NonZeroUsize::new(10_usize).unwrap(),
+        kpis: Vec::new(),
+    };
 
-    let preferences: Arc<Vec<[u8; 3]>> = Arc::new(preference_loading::every_nth(N, 4));
-    let preferences_clone_1 = Arc::clone(&preferences);
-    let preferences_clone_2 = Arc::clone(&preferences);
-    let preferences_clone_3 = Arc::clone(&preferences);
-
-    let mut vote_plurality = [0; 3];
-    let mut vote_score = [0; 3];
-    let mut vote_quadratic = [0; 3];
-
-    let h1 = thread::spawn(move || {
-        vote_plurality = plurality_voting(&preferences_clone_1);
-        println!("{vote_plurality:?}");
-        vote_plurality
-    });
-
-    let h2 = thread::spawn(move || {
-        vote_score = score_voting(&preferences_clone_2);
-        println!("{vote_score:?}");
-        vote_score
-    });
-
-    let h3 = thread::spawn(move || {
-        vote_quadratic = quadratic_voting(&preferences_clone_3);
-        println!("{vote_quadratic:?}");
-        vote_quadratic
-    });
-
-    h1.join().unwrap();
-    h2.join().unwrap();
-    h3.join().unwrap();
+    World::<i32>::simulate();
 }
 
 // Uses preferences to do plurality voting
@@ -87,7 +58,54 @@ struct Predictor {}
 #[derive(Debug)]
 struct World<T: PartialOrd> {
     population: NonZeroUsize,
-    kpis: [T],
+    kpis: Vec<T>,
+}
+
+impl<T: PartialOrd> World<T> {
+    fn simulate() {
+        // Voting population size
+        const N: u32 = 2 * 10_u32.pow(5);
+
+        let preferences: Arc<Vec<[u8; 3]>> = Arc::new(preference_loading::every_nth(N, 4));
+        let preferences_clone_1 = Arc::clone(&preferences);
+        let preferences_clone_2 = Arc::clone(&preferences);
+        let preferences_clone_3 = Arc::clone(&preferences);
+
+        let mut vote_plurality = [0; 3];
+        let mut vote_score = [0; 3];
+        let mut vote_quadratic = [0; 3];
+
+        let h1 = thread::spawn(move || {
+            vote_plurality = plurality_voting(&preferences_clone_1);
+            println!("{vote_plurality:?}");
+            vote_plurality
+        });
+
+        let h2 = thread::spawn(move || {
+            vote_score = score_voting(&preferences_clone_2);
+            println!("{vote_score:?}");
+            vote_score
+        });
+
+        let h3 = thread::spawn(move || {
+            vote_quadratic = quadratic_voting(&preferences_clone_3);
+            println!("{vote_quadratic:?}");
+            vote_quadratic
+        });
+
+        h1.join().unwrap();
+        h2.join().unwrap();
+        h3.join().unwrap();
+    }
+}
+
+trait Cwa {}
+
+impl Cwa for World<i32> {}
+impl World<i32> {
+    fn hi() -> i32 {
+        3
+    }
 }
 
 // fn symmetric_borda_score(preferences: Vec<[usize; 3]>) -> [usize; 3] {}
